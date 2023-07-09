cb7c9f98492b672da3c2b6db0cf330d816e7c41a
ianemiranda <133229803+ianemiranda@users.noreply.github.com>
2023-07-09 16:13:08 +0000

    oi

--- script.js
+++ script.js
@@ -12,9 +12,13 @@ function contar(){
         let f = Number(fim.value)
         let p = Number(passo.value)
 
-        for(let c = i; c <= f; += p) {
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+        }      
+    } else {
+        for(let c = i; c >= f; c -= p) {
             res.innerHTML += ` ${c} \u{1F449}`
         }
-        res.innerHTML += ` ${C} \u{1F3C1}`
-    }
+        res.innerHTML += ` ${c} \u{1F3C1}`
+    } 
 }

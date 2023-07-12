dae036abf8ef4189abb724354fc0df51b8448d84
ianemiranda <133229803+ianemiranda@users.noreply.github.com>
2023-07-12 21:26:19 +0000

    fad

--- tabuada/script.js
+++ tabuada/script.js
@@ -1,32 +1,17 @@
-function contar(){
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossivel contar'
-       //window.alert('[ERRO] Faltam dados.') 
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
     } else {
-        res.innerHTML = 'Contando: <br>'
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (p <= 0) {
-          window.alert('Passo inválido. Considerando PASSO 1')
-    }
-    if (i < f) {
-        //Contagem crescente
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F449}`
-        }      
-    } else {
-        //Contagem regressiva
-        for(let c = i; c >= f; c -= p) {
-            res.innerHTML += ` ${c} \u{1F449}`
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
         }
-    
-    } 
-        res.innerHTML +=  `\u{1F3C1}`
-    } 
+    }
 }
